0593a8a758d7835835b8761b2c34aaa2fafdabcd
Connor Boulton <cjboulton22@gmail.com>
2021-10-26 14:03:47 -0600

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const path = require('path')
+const Rollbar = require('rollbar')
+
+const app = express()
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+    Rollbar.info('html file success')
+})
+
+app.get('/css', function(req, res) {
+    res.sendFile(path.join(__dirname, '../styles.css'))
+})
+
+
+const port = process.env.PORT || 4545
+
+app.listen(port, () => console.log(`running on ${port}`))
