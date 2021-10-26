9af7bc958f2c56261812a48a6ab17c68e401f318
Connor Boulton <cjboulton22@gmail.com>
2021-10-26 14:41:58 -0600

    rollbar

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'))
-    Rollbar.info('html file success')
+    rollbar.info('html file success')
 })
 
 app.get('/css', function(req, res) {
